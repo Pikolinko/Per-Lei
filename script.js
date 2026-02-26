ad5dbe33043f819e6ca2d2cf0ec625bd2ef22097
Pikolinko <113018552+Pikolinko@users.noreply.github.com>
2026-02-26 13:32:39 +0100

    Ultra izgled
    Satro

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
-window.addEventListener("scroll",function(){
-document.querySelector("header").style.background="rgba(0,0,0,0.8)";
+window.addEventListener("scroll",()=>{
+document.querySelector("header").style.background="rgba(0,0,0,0.9)";
 });
+
+function toggleDark(){
+document.body.classList.toggle("dark");
+}
